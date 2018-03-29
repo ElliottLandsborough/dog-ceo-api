1d0d963e7444f3a174028e8a98549d261a647c44
Elliott Landsborough <elliott.landsborough@gmail.com>
2018-03-29 11:14:57 +0100

    Fix bad routes

--- routes.php
+++ routes.php
@@ -11,67 +11,67 @@ $routes = new RouteCollection();
 // list all breed names
 $routes->add('breedList', new Route(
     '/breeds/list',
-    array('filename' => null, 'raw' => false, '_controller' => array(new Apicontroller(), 'breedList')))
-);
+    array('filename' => null, 'raw' => false, '_controller' => array(new Apicontroller(), 'breedList'))
+));
 
 // list all breed names including sub breeds
 $routes->add('breedListAll', new Route(
     '/breeds/list/all',
-    array('filename' => null, 'raw' => false, '_controller' => array(new Apicontroller(), 'breedListAll')))
-);
+    array('filename' => null, 'raw' => false, '_controller' => array(new Apicontroller(), 'breedListAll'))
+));
 
 // list sub breeds
 $routes->add('breedSubList', new Route(
     '/breed/{breed}/list',
-    array('breed' => null, '_controller' => array(new Apicontroller(), 'breedListSub')))
-);
+    array('breed' => null, '_controller' => array(new Apicontroller(), 'breedListSub'))
+));
 
 // random image from all breeds
 $routes->add('breedAllRandom', new Route(
     '/breeds/image/random',
-    array('filename' => null, '_controller' => array(new Apicontroller(), 'breedAllRandomImage')))
-);
+    array('filename' => null, '_controller' => array(new Apicontroller(), 'breedAllRandomImage'))
+));
 
 // get all breed images
 $routes->add('breedAllImages', new Route(
     '/breed/{breed}/images',
-    array('breed' => null, 'breed2' => null, 'all' => true, '_controller' => array(new Apicontroller(), 'breedImage')))
-);
+    array('breed' => null, 'breed2' => null, 'all' => true, '_controller' => array(new Apicontroller(), 'breedImage'))
+));
 
 // get a random image of a breed
 $routes->add('breedRandomImage', new Route(
     '/breed/{breed}/images/random',
-    array('breed' => null, 'breed2' => null, '_controller' => array(new Apicontroller(), 'breedImage')))
-);
+    array('breed' => null, 'breed2' => null, '_controller' => array(new Apicontroller(), 'breedImage'))
+));
 
 // get all images from sub breed
 $routes->add('breedSubAllImages', new Route(
     '/breed/{breed}/{breed2}/images',
-    array('breed' => null, 'breed2' => null, 'all' => true, '_controller' => array(new Apicontroller(), 'breedImage')))
-);
+    array('breed' => null, 'breed2' => null, 'all' => true, '_controller' => array(new Apicontroller(), 'breedImage'))
+));
 
 // get random image from sub breed
 $routes->add('breedSubRandomImage', new Route(
     '/breed/{breed}/{breed2}/images/random',
-    array('breed' => null, 'breed2' => null, '_controller' => array(new Apicontroller(), 'breedImage')))
-);
+    array('breed' => null, 'breed2' => null, '_controller' => array(new Apicontroller(), 'breedImage'))
+));
 
 // get master breed info
 $routes->add('breedText', new Route(
     '/breed/{breed}',
-    array('breed' => null, 'breed2' => null, '_controller' => array(new Apicontroller(), 'breedText')))
-);
+    array('breed' => null, 'breed2' => null, '_controller' => array(new Apicontroller(), 'breedText'))
+));
 
 // get sub breed info
 $routes->add('subBreedText', new Route(
     '/breed/{breed}/{breed2}',
-    array('breed' => null, 'breed2' => null, '_controller' => array(new Apicontroller(), 'breedText')))
-);
+    array('breed' => null, 'breed2' => null, '_controller' => array(new Apicontroller(), 'breedText'))
+));
 
 // get sub breed info
 $routes->add('statsPage', new Route(
     '/stats',
-    array('_controller' => array(new StatsController(), 'statsPage')))
-);
+    array('_controller' => array(new StatsController(), 'statsPage'))
+));
 
 return $routes;
