1ead309700360b7336e705bffda2e869e47c43ff
Elliott Landsborough <elliott.landsborough@gmail.com>
2018-04-11 23:12:20 +0100

    Only use gateway on live

--- routes.php
+++ routes.php
@@ -1,10 +1,13 @@
 <?php
 
-use controllers\ApiController;
-use controllers\ApiControllerGateway;
 use controllers\StatsController;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
+
+$classToUse = 'controllers\ApiController';
+if ($_SERVER['SERVER_NAME'] == 'dog.ceo') {
+    $classToUse = 'controllers\ApiControllerGateway';
+}
 
 // define routes
 $routes = new RouteCollection();
@@ -12,61 +15,61 @@ $routes = new RouteCollection();
 // list all breed names
 $routes->add('breedList', new Route(
     '/breeds/list',
-    array('filename' => null, 'raw' => false, '_controller' => array(new ApiControllerGateway(), 'breedList'))
+    array('filename' => null, 'raw' => false, '_controller' => array(new $classToUse(), 'breedList'))
 ));
 
 // list all breed names including sub breeds
 $routes->add('breedListAll', new Route(
     '/breeds/list/all',
-    array('filename' => null, 'raw' => false, '_controller' => array(new ApiControllerGateway(), 'breedListAll'))
+    array('filename' => null, 'raw' => false, '_controller' => array(new $classToUse(), 'breedListAll'))
 ));
 
 // list sub breeds
 $routes->add('breedSubList', new Route(
     '/breed/{breed}/list',
-    array('breed' => null, '_controller' => array(new ApiControllerGateway(), 'breedListSub'))
+    array('breed' => null, '_controller' => array(new $classToUse(), 'breedListSub'))
 ));
 
 // random image from all breeds
 $routes->add('breedAllRandom', new Route(
     '/breeds/image/random',
-    array('filename' => null, '_controller' => array(new ApiControllerGateway(), 'breedAllRandomImage'))
+    array('filename' => null, '_controller' => array(new $classToUse(), 'breedAllRandomImage'))
 ));
 
 // get all breed images
 $routes->add('breedAllImages', new Route(
     '/breed/{breed}/images',
-    array('breed' => null, 'breed2' => null, 'all' => true, '_controller' => array(new ApiControllerGateway(), 'breedImage'))
+    array('breed' => null, 'breed2' => null, 'all' => true, '_controller' => array(new $classToUse(), 'breedImage'))
 ));
 
 // get a random image of a breed
 $routes->add('breedRandomImage', new Route(
     '/breed/{breed}/images/random',
-    array('breed' => null, 'breed2' => null, '_controller' => array(new ApiControllerGateway(), 'breedImage'))
+    array('breed' => null, 'breed2' => null, '_controller' => array(new $classToUse(), 'breedImage'))
 ));
 
 // get all images from sub breed
 $routes->add('breedSubAllImages', new Route(
     '/breed/{breed}/{breed2}/images',
-    array('breed' => null, 'breed2' => null, 'all' => true, '_controller' => array(new ApiControllerGateway(), 'breedImage'))
+    array('breed' => null, 'breed2' => null, 'all' => true, '_controller' => array(new $classToUse(), 'breedImage'))
 ));
 
 // get random image from sub breed
 $routes->add('breedSubRandomImage', new Route(
     '/breed/{breed}/{breed2}/images/random',
-    array('breed' => null, 'breed2' => null, '_controller' => array(new ApiControllerGateway(), 'breedImage'))
+    array('breed' => null, 'breed2' => null, '_controller' => array(new $classToUse(), 'breedImage'))
 ));
 
 // get master breed info
 $routes->add('breedText', new Route(
     '/breed/{breed}',
-    array('breed' => null, 'breed2' => null, '_controller' => array(new ApiControllerGateway(), 'breedText'))
+    array('breed' => null, 'breed2' => null, '_controller' => array(new $classToUse(), 'breedText'))
 ));
 
 // get sub breed info
 $routes->add('subBreedText', new Route(
     '/breed/{breed}/{breed2}',
-    array('breed' => null, 'breed2' => null, '_controller' => array(new ApiControllerGateway(), 'breedText'))
+    array('breed' => null, 'breed2' => null, '_controller' => array(new $classToUse(), 'breedText'))
 ));
 
 // get sub breed info
