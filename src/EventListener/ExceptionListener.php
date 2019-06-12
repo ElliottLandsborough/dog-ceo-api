0d163a127fecd5cb9053319dad06e0c6405499e7
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-06-12 11:07:40 +0100

    Custom exception listener

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,37 @@
+<?php
+// src/EventListener/ExceptionListener.php
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        // You get the exception object from the received event
+        $exception = $event->getException();
+        $message = sprintf(
+            'My Error says: %s with code: %s',
+            $exception->getMessage(),
+            $exception->getCode()
+        );
+
+        // Customize your response object to display the exception details
+        $response = new Response();
+        $response->setContent($message);
+
+        // HttpExceptionInterface is a special type of exception that
+        // holds status code and header details
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        // sends the modified response object to the event
+        $event->setResponse($response);
+    }
+}
