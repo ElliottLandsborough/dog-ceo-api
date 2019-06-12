8619b9d64953baabac0132f7df937b7640f915fc
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-06-12 11:15:26 +0100

    Always return json with correct headers during exceptions

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -13,14 +13,13 @@ class ExceptionListener
         // You get the exception object from the received event
         $exception = $event->getException();
         $message = sprintf(
-            'My Error says: %s with code: %s',
+            '%s with code: %s',
             $exception->getMessage(),
             $exception->getCode()
         );
 
         // Customize your response object to display the exception details
         $response = new Response();
-        $response->setContent($message);
 
         // HttpExceptionInterface is a special type of exception that
         // holds status code and header details
@@ -30,6 +29,15 @@ class ExceptionListener
         } else {
             $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        $response->setContent(json_encode((object) [
+            'status' => 'error',
+            'message'   => $message,
+            'code' => $response->getStatusCode(),
+        ]));
 
         // sends the modified response object to the event
         $event->setResponse($response);
