a7c3b7685637843f5dc967c1f34359b2fb3507a0
Elliott Landsborough <elliott.landsborough@gmail.com>
2022-08-09 08:50:21 +0100

    More symphony 6 default files

--- public/index.php
+++ public/index.php
@@ -1,15 +1,8 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\ErrorHandler\Debug;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
-
-    Debug::enable();
-}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
