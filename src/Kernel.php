c6d10bb690a1216475fa16c08096255dfe89ba1e
Elliott Landsborough <elliott.landsborough@gmail.com>
2025-10-02 20:22:47 +0100

    Restore original kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,21 +8,4 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    /**
-     * Debug: Log when Kernel is loaded and from where.
-     */
-    public static function debugLoadedFrom()
-    {
-        if (true) {
-            error_log('App\\Kernel loaded from: ' . __FILE__ . ' (realpath: ' . realpath(__FILE__) . ')');
-            error_log("Backtrace:\n" . print_r(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS), true));
-        }
-    }
-
-    // Static property triggers debug output on file load
-    public static $debugInit;
 }
-
-// Trigger debug output when file is loaded
-\App\Kernel::$debugInit = \App\Kernel::debugLoadedFrom();
